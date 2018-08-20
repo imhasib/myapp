d3be31c7ab4ad309c361d17c71271fa035876a11
Hasib <email2hasib@gmail.com>
2018-08-20 18:31:52 +0600

    index 2 file added

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var path = require('path');
 const PORT = process.env.PORT || 3000;
 
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(path.join(__dirname , '/views')));
 //Store all HTML files in view folder.
 // app.use(express.static("scripts"));
 //Store all JS and CSS in Scripts folder.
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
+});
+app.get('/index2', function (req, res) {
+  res.sendFile(__dirname + '/views/index_2.html');
 });
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT + '!');
